d5ea8473b56f79979b35a670bf0f6cd75fee2865
William Kopaczewski <w_kopa@yahoo.com>
2025-07-03 02:11:27 -0400

    Added function to read max of 10,000 characters from given file

--- tests.py
+++ tests.py
@@ -1,12 +1,18 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def tests():
     working_directory = "calculator"
     tests = []
-    tests.append(get_files_info(working_directory, "."))
-    tests.append(get_files_info(working_directory, "pkg"))
-    tests.append(get_files_info(working_directory, "/bin"))
-    tests.append(get_files_info(working_directory, "../"))
+
+    tests.append(get_file_content(working_directory, "main.py"))
+    tests.append(get_file_content(working_directory, "pkg/calculator.py"))
+    tests.append(get_file_content(working_directory, "/bin/cat"))
+    tests.append(get_file_content(working_directory, "lorem.txt"))
+    # tests.append(get_files_info(working_directory, "."))
+    # tests.append(get_files_info(working_directory, "pkg"))
+    # tests.append(get_files_info(working_directory, "/bin"))
+    # tests.append(get_files_info(working_directory, "../"))
 
     for i, test, in enumerate(tests):
         print_tests(test, i+1)
