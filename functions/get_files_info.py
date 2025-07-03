633b8567e922276c8378771db6c6856b44a6794a
William Kopaczewski <w_kopa@yahoo.com>
2025-07-03 07:17:54 -0400

    Fixed bug that caused get_files_info to fail if no directory was specified

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,26 +2,29 @@ import os
 from google.genai import types
 
 def get_files_info(working_directory, directory=None):
-    path = os.path.join(working_directory, directory) 
+    abs_working_path = os.path.abspath(working_directory)
+    abs_path = abs_working_path
+    if directory:
+        abs_path = os.path.abspath(os.path.join(working_directory, directory))
 
-    if os.path.abspath(working_directory) not in os.path.abspath(path):
+    if abs_working_path not in abs_path:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
-    if not os.path.isdir(path):
+    if not os.path.isdir(abs_path):
         return f'Error: "{directory}" is not a directory'
     
     string_list = []
-    items = os.listdir(path)
+    items = os.listdir(abs_path)
     try:
         for item in items:
-            string_list.append(get_info(os.path.join(path, item), item))
+            string_list.append(get_info(os.path.join(abs_path, item), item))
 
         return "\n".join(string_list)
     except Exception as e:
         return f"Error listing files: {e}"
     
-def get_info(path, file_name):
-    return f"- {file_name}: file_size={os.path.getsize(path)}, is_dir={os.path.isdir(path)}"
+def get_info(abs_path, file_name):
+    return f"- {file_name}: file_size={os.path.getsize(abs_path)}, is_dir={os.path.isdir(abs_path)}"
 
 schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
